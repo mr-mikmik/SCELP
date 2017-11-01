628a4674b60c8dfeb3a8bc4373b6c6d19eacf54f
mr-mikmik <aglaner@hotmail.com>
2017-11-01 09:25:58 +0100

    adding peelist

--- Spherical_Codebook.py
+++ Spherical_Codebook.py
@@ -1,7 +1,8 @@
-import numpy
+import numpy as np
 import math
 
 class S_Codebook():
+
     def __init__(self, Lv, N_sp, M_r ):
         """
         Inicialize the codebook
@@ -24,9 +25,40 @@ class S_Codebook():
         self.centroids = []
         self.centroids_count = 0
         self.c_indx_to_coords = {}
-        self.init_Centroids(self.centroids, self.c_indx_to_coords)
+        _, self.peelist = self.init_Centroids(self.centroids, self.c_indx_to_coords)
         self.c_indx_to_cart = self.init_cartesians_dic()
         self.c_indx_to_angles = self.init_angle_dic()
+
+
+    def preselection(self, d0):
+        """
+        Gets the candidates to be quantized given the vector d0. To do so w
+        :param d0: <[floats]> vector to be quantized
+        :return: <[int]> a list containing the indexes (int) referring to the candidates centroids
+        """
+        candidates = []
+        # 1 - Normalize the vector
+        d0_norm = np.linalg.norm(d0)
+        c0 = d0/d0_norm
+        # 2 - Convert the vector to spherical coordinates
+        _, sph_c0 = self.cartesian2spherical(c0)
+        # 3 - Select the candidiates making use of the peelist
+        peeling_centroids = self.centroids
+        def search(value,list):
+            cand = []
+            for i in len(list):
+                if i
+            returnn cand
+        for i, phi in enumerate(sph_c0):
+
+
+        return candidates
+
+
+
+
+
+    ## INITIALIZE FUNCTIONS:
 
     def init_Centroids(self, c, d, lv_i=0,previous=()):
         """
@@ -40,11 +72,14 @@ class S_Codebook():
         """
         # Number of angles -> Lv - 1
         if lv_i < self.Lv - 2 :
+            peelist = []
             for i in range(self.N_sp):
                 c.append([])
+                phi_0 = (i * 0.5) * self.theta
+                peelist.append([phi_0,None])
                 # Call the same function to compute recursively
-                c[i] = self.init_Centroids(c[i], d, lv_i+1, previous=previous+(i,))
-            return c
+                c[i], peelist[i][1] = self.init_Centroids(c[i], d,lv_i+1, previous=previous+(i,))
+            return c, peelist
         else:
             # The last layer of the apple peeling method
             phi_p = (previous[-1] + 0.5)*self.theta
@@ -54,7 +89,9 @@ class S_Codebook():
                 coords_actual_layer.append(self.centroids_count)
                 d[self.centroids_count] = previous + (i,)
                 self.centroids_count += 1
-            return coords_actual_layer
+            return coords_actual_layer, [phi_p, coords_actual_layer]
+
+
 
     def init_cartesians_dic(self):
         """
@@ -98,12 +135,9 @@ class S_Codebook():
                 a = (c + 0.5)*self.theta
                 angles += (a,)
             phi_p = (coords[-1] + 0.5) * 2 * math.pi / self.get_Nspl((coords[-2] + 0.5) * self.theta)
-            angles += (phi_p)
+            angles += (phi_p,)
             angle_dic[k] = angles
         return angle_dic
-
-
-
 
     def init_sin_dic(self):
         """
@@ -129,6 +163,9 @@ class S_Codebook():
             d[i] = math.cos((i + 0.5)*self.theta)
         return d
 
+
+    ## GETTERS & AUXILIAR FUNCTIONS:
+
     def get_Nspl(self, phi_i):
         """
         Auxiliar function to compute the number of centroids in the last layer
@@ -147,3 +184,26 @@ class S_Codebook():
 
     def get_cartesians_dic(self):
         return self.c_indx_to_cart
+
+
+    def cartesian2spherical(self, c0):
+        """
+        Converts the vector to is spherical equivalent
+        :param c0: cartesian n-dimentional vector
+        :return: <float, (float)> modulus, n-1-dimentional list containing the spherical coordinates
+        """
+        sph_coords = ()     #Vector containg the angles
+        modulus = np.linalg.norm(c0)
+        for i, c in enumerate(c0[:-2]):
+            mod_i = np.linalg.norm(c0[i:])
+            phi_i = math.acos(c/mod_i)
+            sph_coords += (phi_i,)
+        # Last angle:
+        if c0[-1]>=0:
+            phi_l = math.acos(c0[-2]/np.linalg(c0[-2:]))
+        else:
+            phi_l = 2*math.pi-math.acos(c0[-2] / np.linalg(c0[-2:]))
+        sph_coords += (phi_l,)
+
+        return modulus, sph_coords
+
