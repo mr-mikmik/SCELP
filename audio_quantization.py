95bdc70dd7426957c8f9a59fe5e8b40a5d2d950a
mr-mikmik <aglaner@hotmail.com>
2017-11-04 11:54:39 +0100

    audio to int solved

--- audio_quantization.py
+++ audio_quantization.py
@@ -1,13 +1,26 @@
+# *-* coding: utf-8 *-*
+
 import wave
 import os.path
+import codecs
 
 audio_file_name = 'Audio_cav.wav'
 audio_path = os.path.join('./audio', audio_file_name)
 
-with wave.open(audio_path, 'r') as audio_reader:
-    sample_width = audio_reader.getsampwidth()
-    print sample_width
+audio_reader = wave.open(audio_path, 'r')
 
-    audio_reader.close()
+sample_width = audio_reader.getsampwidth()
+print sample_width
+framerate = audio_reader.getframerate()
+print framerate
+frame_number = audio_reader.getnframes()
+print frame_number
+
+for i in range(1000):
+    frames = audio_reader.readframes(20)
+    b = int(codecs.encode(frames, 'hex'), 16)#int.from_bytes(frames)
+    print b
+
+audio_reader.close()
 
 
